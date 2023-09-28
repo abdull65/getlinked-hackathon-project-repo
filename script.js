013564a8c9165b582c3a67f901494d8d5af1bba8
Abdullahi Mohammed <odanwu211@Gmail.com>
2023-09-28 23:29:55 +0100

    update on responsiveness

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 const menuBtnEl = document.querySelector(".navMenuBox");
 const navBarEl = document.querySelector(".navBar");
 const closeMenuEl = document.querySelector("#closeBtn");
-const bodyEl = document.querySelector("#body");
+const bodyEl = document.querySelector("body");
+const navLinkEl = document.querySelectorAll(".navLink");
 
-menuBtnEl.addEventListener("click", () => {
+menuBtnEl.addEventListener("click", (event) => {
+  // Prevent the click event from propagating to the body element
+  event.stopPropagation();
   navBarEl.classList.toggle("openMenu");
   closeMenuEl.classList.toggle("openMenu");
   bodyEl.classList.toggle("openMenu");
+});
+
+bodyEl.addEventListener("click", () => {
+  if (navBarEl.classList.contains("openMenu")) {
+    navBarEl.classList.remove("openMenu");
+    closeMenuEl.classList.toggle("openMenu");
+    bodyEl.classList.remove("openMenu");
+  }
+});
+navLinkEl.forEach((link) => {
+  if (navBarEl.classList.contains("openMenu")) {
+    link.addEventListener("click", () => {
+      navBarEl.classList.remove("openMenu");
+      closeMenuEl.classList.toggle("openMenu");
+      bodyEl.classList.remove("openMenu");
+    });
+  }
 });
