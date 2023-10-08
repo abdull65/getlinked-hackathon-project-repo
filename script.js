0e9b05d7b36f99a7c59a48c20bc82e47ac7edbf1
Abdullahi Mohammed <odanwu211@Gmail.com>
2023-10-08 08:14:48 +0100

    update

--- script.js
+++ script.js
@@ -90,22 +90,103 @@ const regBtnEl = document.querySelectorAll("#regBtn");
 const regBtnListEl = document.querySelector("#regBtnList");
 const resetBtnEl = document.querySelector("#resetBtn");
 const popUpEl = document.querySelector(".popUp");
+const nameEl = document.getElementById("name");
+const emailEl = document.getElementById("email");
+const phoneEl = document.getElementById("phone");
+const categoryEl = document.getElementById("category");
+const groupSizeEl = document.getElementById("group_size");
+const topicEl = document.getElementById("project_topic");
+const inputEl = document.querySelectorAll(".regInput");
+
 const submitFormBtn = document.getElementById("submitFormBtn");
 const regSectionEl = document.getElementById("registerSection");
+
 regBtnEl.forEach((regBtn) => {
   regBtn.addEventListener("click", () => {
     regSectionEl.classList.add("showReg");
     regBtnListEl.classList.add("showReg");
-    // regSectionEl.classList.add("slide");
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
+
   submitFormBtn.addEventListener("click", () => {
+    inputEl.forEach((input) => {
+      if (input.value === "") {
+        input.classList.add("warn");
+        return;
+      } else {
+        input.classList.remove("warn");
+      }
+    });
+    // Validate name (letters and spaces only)
+    const nameRegex = /^[A-Za-z ]+$/;
+    if (!nameRegex.test(nameEl.value)) {
+      nameEl.classList.add("warn");
+      // alert("Invalid name");
+      return;
+    } else {
+      nameEl.classList.remove("warn");
+    }
+
+    // Validate group size (check if an option is selected)
+    if (groupSizeEl.value === "") {
+      groupSizeEl.classList.add("warn");
+      // alert("Please select a group size");
+      return;
+    } else {
+      groupSizeEl.classList.remove("warn");
+    }
+
+    // Validate category (check if an option is selected)
+    if (categoryEl.value === "") {
+      categoryEl.classList.add("warn");
+      // alert("Please select a category");
+      return;
+    } else {
+      categoryEl.classList.remove("warn");
+    }
+
+    // Validate email using regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailEl.value)) {
+      emailEl.classList.add("warn");
+      // alert("Invalid email address");
+      return;
+    } else {
+      emailEl.classList.remove("warn");
+    }
+
+    // Validate phone number using regex (assuming 10-digit US format)
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phoneEl.value || phoneEl === "")) {
+      phoneEl.classList.add("warn");
+      // alert("Invalid phone number");
+      return;
+    } else {
+      phoneEl.classList.remove("warn");
+    }
+
+    // Show popup
     popUpEl.style.visibility = "visible";
     popUpEl.classList.add("popUp_animation");
     popUpEl.addEventListener("animationend", () => {
       popUpEl.classList.remove("popUp_animation");
     });
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   });
+
   resetBtnEl.addEventListener("click", () => {
     location.reload();
+  });
+});
+document.querySelector(".backToTopBtn").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
